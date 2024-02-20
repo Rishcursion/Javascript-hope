094edeb45556d5d062137b8dd7b9b2d8e5f2bd53
HotStuff <game20041608@gmail.com>
2024-02-21 01:02:38 +0530

    cleaning up css

--- public/backend/server.js
+++ public/backend/server.js
@@ -1,5 +1,6 @@
 //Defining Dependencies
-
+//Used for hashing passwords
+const bcrypt = require('bcrypt');
 //Used for starting server
 const express = require('express');
 //Used to query paths for project file
@@ -8,22 +9,62 @@ const path = require('path');
 const bodyparser = require('body-parser');
 //Helps with mysql connectivity
 const knex = require('knex');
+const { password } = require('pg/lib/defaults');
 
 //setting up db connection
+const db = knex({
+    client:'mariadb',
+    user : 'hotstuff',
+    password : 'sexy',
+    database: 'Workout'
 
+})
 
+const init_id = 1000;
 //starting server with express
 const app = express();
 
 //assigning path variable
-let initialPath = path.join(__dirname,"public");
+let initialPath = path.join(__dirname,"public/frontend");
+
 app.use(bodyparser.json());
 app.use(express.static(initialPath));
-app.get('/',(req,res) =>{
-    res.sendFile(path.join(initialPath,"index.html"));
+
+
+app.get('/login',(req,res) =>{
+    res.sendFile(path.join(initialPath,"html/login.html"));
+})
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(initialPath,"html/register.html"));
+})
+
+app.post('/register-user',(req,res)=>{
+    const {name,email,password,repass} = req.body;
+    const id = (init_id + 1)% 1000 +67;
+    if(password != repass){
+        console.log("Passwords Do Not Match!!");
+    }
+    else{
+        db("Users").insert({
+            ID: id,
+            name:name,
+            email:email,
+            password: password
+        })
+        .returning(["name","email"])
+        .then(data => {
+            res.json(data[0])
+        })
+        .catch((err) => {
+            if (err.detail.includes('already exists')){
+                res.json("Email Already Registerd");
+            }
+        })
+    }
 
 })
-app.get('')
+
 app.listen(3000,(req,res) =>{
-``
+    console.log(`Listening On port 300...`);
 });
