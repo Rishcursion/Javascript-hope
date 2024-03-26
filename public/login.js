5bfb8d9c322aa5ccbb031abb3cfc83fc7e879019
HotStuff <game20041608@gmail.com>
2024-03-26 22:38:54 +0530

    formatted js code

--- public/login.js
+++ public/login.js
@@ -1,38 +1,42 @@
 //Dependencies
-const express= require('express');
+const express = require("express");
 const mariadb = require("mariadb");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const path = require("path");
-const bcrypt = require('bcrypt');
-const http = require('http');
-const bodyParser = require('body-parser');
+const bcrypt = require("bcrypt");
+const http = require("http");
+const bodyParser = require("body-parser");
+const validator = require("express-validator");
+const { error } = require("console");
 
 //setting up variables
 const app = express();
+const port = 3000;
 
 //getting db creds from env file:
 dotenv.config();
 
 //setting up express.js
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static( 'public'));2
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
 //setting up GET requests
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/static/landingpage.html'))
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/static/landingpage.html"));
+});
 
-app.get('/login',(req,res) =>{
-    res.sendFile(path.join(__dirname,'/static/login.html'))
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/static/login.html"));
 });
-app.get('/login/register',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/static/register.html'))
+app.get("/login/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/static/register.html"));
 });
+
 //setting up POST requests
-app.post('/login',(req,res)=>{
-    let username = req.body.Username;
-    let password = req.body.pass;
+
+app.post("/login", (req, res) => {
+let  username = req.body.Username;
+let  password = req.body.pass;
 });
 
-
-const port = 3000;
-app.listen(port, () => console.log('Listening on port ${port}'));
+app.listen(port, () => console.log(`Listening on port: ${port}`));
